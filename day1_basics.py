13c687a1a757ebc945fdb15379d54fa80a95b874
Aiman Shaikh <heybro2426@gmail.com>
2026-02-16 19:41:45 +0530

    Add Day 1 Python basics practice

---
+++ day1_basics.py
@@ -0,0 +1,14 @@
+# Day -1 Python Basics
+
+print("hello, my  name is Aiman")
+
+
+# Simple addition
+a = 10
+b = 5
+print("Sum:", a + b)
+
+
+# Loop example
+for i in range(1,6):
+  print("Number:", i)
