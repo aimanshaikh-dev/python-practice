b001e55ea2f223e9239eeec6584b5a57c7822c88
Aiman Shaikh <heybro2426@gmail.com>
2026-02-19 20:38:25 +0530

    add functions and modules master practice

---
+++ function_master_practice.py
@@ -0,0 +1,56 @@
+# Even or Odd (using return)
+
+def even_or_odd(n):
+    if n % 2 == 0:
+        return "Even"
+    else:
+        return "Odd"
+    
+print("Even or Odd:", even_or_odd(8))
+
+
+# Biggest of 3 numbers (without max())
+
+def biggest(a,b,c):
+    if a>=b and a>=c:
+        return a
+    if b>=a and b>=c:
+        return b
+    else:
+        return c
+      
+print("Biggest:" , biggest(10, 20, 30))
+
+
+# Factorial usinf recursion
+
+def factorial(n):
+    if n == 2:
+        return 1
+    return n * factorial(n-1)
+
+print("Factorial:", factorial(2))
+
+
+# lambda function
+
+square  = lambda x: x*x
+print("Square:", square(5))
+
+
+# Using math Module
+
+import math
+print(math.sqrt(16))
+
+
+# Variable scope example
+
+x =10 #Global variable
+
+def show_scope():
+    x = 6 #Local variable
+    print("Inside function:", x)
+
+show_scope()
+print("Outside function:", x)
