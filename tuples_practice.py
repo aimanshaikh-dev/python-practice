0e3cb4eaf9dafad68c1a673326410bd9a6e368fc
Aiman Shaikh <heybro2426@gmail.com>
2026-02-25 19:53:04 +0530

    Add tuples pracitce
    Removed set operations and related print statements.

--- tuples_practice.py
+++ tuples_practice.py
@@ -12,22 +12,3 @@ print("Lenght:", len(t))
 print("Count of 20:", t.count(20))
 print("index of 30:", t.index(30))
 
-# Create sets
-
-a ={1, 2, 3, 4}
-b ={3, 4, 5, 6}
-
-print("Set A:", a)
-print("Set b:", b)
-
-# Union , Intersection , Difference
-
-print("Union of:", a.union(b))
-print("Intersection of:", a.intersection(b))
-print("Difference of:", a.difference(b))
-
-# Add and Remove
-
-a.add(7)
-a.remove(4)
-print("Updated A:", a)
